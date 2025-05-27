80edb12ed86a8cddbb299e7379eadfac069c9e3f
Kirsten Rauffer <krauffer@ulta.com>
2025-05-26 21:05:35 -0500

    Working sprites yayyy

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,16 @@
-import sprite1 from '../assets/face/1.png'; 
-import sprite2 from '../assets/face/2.png'; 
-import sprite3 from '../assets/face/3.png'; 
-import sprite4 from '../assets/face/4.png'; 
-import sprite5 from '../assets/face/5.png'; 
-import sprite6 from '../assets/face/6.png'; 
-import sprite7 from '../assets/face/7.png'; 
-import sprite8 from '../assets/face/8.png'; 
-import sprite9 from '../assets/face/9.png'; 
-import sprite10 from '../assets/face/10.png'; 
-
 const sprites = [
-    sprite1,
-    sprite2,
-    sprite3,
-    sprite4,
-    sprite5,
-    sprite6,
-    sprite7,
-    sprite8,
-    sprite9,
-    sprite10
+    "./assets/soot_sprites/face/1.png",
+    "./assets/soot_sprites/face/2.png",
+    "./assets/soot_sprites/face/3.png",
+    "./assets/soot_sprites/face/4.png",
+    "./assets/soot_sprites/face/5.png",
+    "./assets/soot_sprites/face/6.png",
+    "./assets/soot_sprites/face/7.png",
+    "./assets/soot_sprites/face/8.png",
+    "./assets/soot_sprites/face/9.png",
+    "./assets/soot_sprites/face/10.png"
 ]
 const canvas = document.querySelector("#sprite-house");
-const addButton = document.querySelector("#add-button");
 
 // module aliases
 let Engine = Matter.Engine,
@@ -36,6 +24,11 @@ let Engine = Matter.Engine,
 // create an engine
 let engine = Engine.create();
 
+const SPRIZE_SIZE = 70;
+const BOUNDARY_SIZE = 20;
+const HEIGHT = 640 + BOUNDARY_SIZE;
+
+
 // create a renderer
 let render = Render.create({
   canvas: canvas,
@@ -44,32 +37,53 @@ let render = Render.create({
     background: "transparent",
     wireframes: false,
     width: window.innerWidth,
-    height: window.innerHeight
+    height: HEIGHT,
+    pixelRatio: 'auto'
   }
 });
 
 const createObject = () => {
-  let box = Bodies.circle(window.innerWidth / 2, 0, 40, {
+  let box = Bodies.circle(Math.random() * (SPRIZE_SIZE - window.innerWidth) + window.innerWidth, -SPRIZE_SIZE, SPRIZE_SIZE, {
     render: {
       sprite: {
-        texture: sprites[Math.floor(Math.random() * sprites.length)]
+        texture: sprites[Math.floor(Math.random() * sprites.length)],
+        xScale: 0.035,
+        yScale: 0.035
       }
     }
   });
   Composite.add(engine.world, box);
 };
 
+const GENERATE_INTERVAL = 500;
+const intervalId = setInterval(createObject, GENERATE_INTERVAL);
+
+setTimeout(() => {
+  clearInterval(intervalId);
+}, GENERATE_INTERVAL*13);
+
+
 // create ground
-let ground = Bodies.rectangle(
+const ground = Bodies.rectangle(
   window.innerWidth / 2,
-  window.innerHeight,
+  HEIGHT,
   window.innerWidth,
-  60,
-  { isStatic: true }
+  BOUNDARY_SIZE,
+  {
+    isStatic: true,
+    label: "Ground"
+   }
 );
 
+const wallLeft = Bodies.rectangle(-BOUNDARY_SIZE, HEIGHT / 2, BOUNDARY_SIZE, HEIGHT, {
+  isStatic: true, label: "Wall Left"
+});
+const wallRight = Bodies.rectangle(window.innerWidth + BOUNDARY_SIZE, HEIGHT / 2, BOUNDARY_SIZE, HEIGHT, {
+  isStatic: true, label: "Wall Right"
+});
+
 // add all of the bodies to the world
-Composite.add(engine.world, [ground]);
+Composite.add(engine.world, [ground, wallLeft, wallRight]);
 
 // run the renderer
 Render.run(render);
@@ -80,13 +94,11 @@ let runner = Runner.create();
 // run the engine
 Runner.run(runner, engine);
 
-addButton.addEventListener("click", createObject);
-
 let mouse = Mouse.create(render.canvas);
 let mouseConstraint = MouseConstraint.create(engine, {
   mouse: mouse,
   constraint: {
-    stiffness: 0.2,
+    stiffness: 1,
     render: {
       visible: false
     }
@@ -94,4 +106,9 @@ let mouseConstraint = MouseConstraint.create(engine, {
 });
 
 Composite.add(engine.world, mouseConstraint);
+
+window.addEventListener("resize", function () {
+  Matter.Body.setPosition(ground, {x: window.innerWidth / 2, y: HEIGHT + BOUNDARY_SIZE})
+  Matter.Body.setPosition(wallRight, {x: window.innerWidth + BOUNDARY_SIZE, y: HEIGHT / 2})
+});
 
