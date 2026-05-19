0183cc091f28c9aa2b220ccdeda4d9e136b9c7d7
kirstenandizzy <kirsten.rauffer+1@gmail.com>
2026-05-19 16:36:18 -0500

    modal

--- src/components/FAQModal.jsx
+++ src/components/FAQModal.jsx
@@ -11,6 +11,10 @@ const FAQ_ITEMS = [
     answer: (<><strong>Windows on the Water</strong><br />931 Ocean Ave<br />Sea Bright, NJ 07760<br /><br />The ceremony will be outdoors weather permitting, and the rest of the evening will be outdoors/indoors at your leisure.</>)
   },
   {
+    question: `Children... what's the deal?`,
+    answer: 'Kids are welcome, though we are not guaranteeing that the wedding is "child-friendly" in regards to things like music selection. There will be a menu specific for children under 12.'
+  },
+  {
     question: 'Do you have a gift registry?',
     answer: 'We do not have a registry. We mean it when we say that your presence at our wedding is the greatest gift we could ask for, and we can’t wait to celebrate with you! If you did want to gift us something, we are asking for cash gifts to go towards our honeymoon and future adventures!'
   },
@@ -29,10 +33,6 @@ const FAQ_ITEMS = [
   {
     question: 'What time does the bus leave the venue to the hotel?',
     answer: 'The bus boards at 11:00 PM, and will leave at 11:15 PM to drop off guests at the Sonesta Select Hotel.'
-  },
-  {
-    question: 'Are children invited?',
-    answer: 'Children are welcome.'
   },
   {
     question: 'Will dinner be served?',
