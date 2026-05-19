36674f138d6a8f493952b6b5b8be8bd7916db720
kirstenandizzy <kirsten.rauffer+1@gmail.com>
2026-05-19 16:39:58 -0500

    Update FAQModal.jsx

--- src/components/FAQModal.jsx
+++ src/components/FAQModal.jsx
@@ -15,16 +15,20 @@ const FAQ_ITEMS = [
     answer: 'Kids are welcome, though we are not guaranteeing that the wedding is "child-friendly" in regards to things like music selection. There will be a menu specific for children under 12.'
   },
   {
+    question: 'Dress code?',
+    answer: 'Formal, colorful attire encouraged! The ceremony will be outdoors on the beach (weather permitting) so prioritize your comfort. We will move inside after the ceremony, but you can still enjoy the beach and the balconies to your liking throughout.'
+  },
+  {
     question: 'Do you have a gift registry?',
     answer: 'We do not have a registry. We mean it when we say that your presence at our wedding is the greatest gift we could ask for, and we can’t wait to celebrate with you! If you did want to gift us something, we are asking for cash gifts to go towards our honeymoon and future adventures!'
   },
   {
-    question: 'What is the dress code?',
-    answer: 'Formal, colorful attire encouraged! The ceremony will be outdoors on the beach (weather permitting) so prioritize your comfort. We will move inside after the ceremony, but you can still enjoy the beach and the balconies to your liking throughout.'
-  },
-  {
     question: 'What time should I arrive?',
     answer: 'The ceremony begins at 5:30 PM. Doors will open at 5:00 PM, and we recommend arriving early to find parking and get settled before the ceremony starts.'
+  },
+  {
+    question: 'What is the parking situation?',
+    answer: 'There is free parking available at the venue.'
   },
   {
     question: 'What time does the bus leave the hotel for the venue?',
@@ -32,15 +36,7 @@ const FAQ_ITEMS = [
   },
   {
     question: 'What time does the bus leave the venue to the hotel?',
-    answer: 'The bus boards at 11:00 PM, and will leave at 11:15 PM to drop off guests at the Sonesta Select Hotel.'
-  },
-  {
-    question: 'Will dinner be served?',
-    answer: 'Absolutely! Dinner will be served at around 7:00 PM, followed by dessert and dancing.'
-  },
-  {
-    question: 'What is the parking situation?',
-    answer: 'There is free parking available at the venue.'
+    answer: 'There will be 2 departures to drop off guests at the Sonesta Select Hotel at 10:00 PM and 11:15 PM.'
   },
 ];
 
